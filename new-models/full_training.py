9e94ff70328537a8b7d30634e994f23f52e2d250
Varun Vejalla <varun.vejalla@gmail.com>
2025-04-23 11:12:25 -0400

    new model

---
+++ new-models/full_training.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+N = 2
+class CostNet(nn.Module):
+    def __init__(self, state_dim=4*N, action_dim=m, cost_embed_dim=k):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim + action_dim + cost_embed_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, state, action, cost_embed):
+        x = torch.cat([state, action, cost_embed], dim=-1)
+        return self.net(x)  # (batch_size, 1)
+
+class PolicyNet(nn.Module):
+    def __init__(self, state_dim, cost_dim, action_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim + cost_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, action_dim)
+        )
+
+    def forward(self, s_t, c_i):
+        x = torch.cat([s_t, c_i], dim=-1)
+        return self.net(x)  # returns a_t^i
+
+
+class DynamicsNet(nn.Module):
+    def __init__(self, state_dim=4*N, action_dim=m*N):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim + action_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, state_dim)
+        )
+
+    def forward(self, state, actions):
+        x = torch.cat([state, actions], dim=-1)
+        return self.net(x)
+
+def training_step(trajectory_batch, cost_net, dynamics_net, policy_net, cost_embeddings, entropy_weight, ece_weight, accel_weight, embed_weight):
+    """
+    trajectory_batch: dict of tensors with keys:
+        - states: (B, T, N, 4)
+        - next_states: (B, T, N, 4)
+    """
+    states = trajectory_batch['states']      # (B, T, N, 4)
+    next_states = trajectory_batch['next_states']  # (B, T, N, 4)
+
+    B, T, N, _ = states.shape
+    device = states.device
+
+    total_dyn_loss = 0.
+    total_ece_loss = 0.
+    total_accel_loss = 0.
+
+    for t in range(T - 1):
+        s_t = states[:, t]
+        s_tp1 = next_states[:, t]
+
+        predicted_actions = []
+        for i in range(N):
+            s_i_t = s_t[:, i]                  # (B, 4)
+            c_i = cost_embeddings[i].unsqueeze(0).expand(B, -1)  # (B, k)
+
+            a_i_t = policy_net(s_i_t, c_i)     # (B, m)
+            predicted_actions.append(a_i_t)
+
+        actions = torch.stack(predicted_actions, dim=1)  # (B, N, m)
+        actions_flat = actions.view(B, -1)               # (B, N*m)
+
+        s_t_flat = s_t.view(B, -1)
+        s_pred_tp1 = dynamics_net(s_t_flat, actions_flat)
+        s_true_tp1 = s_tp1.view(B, -1)
+        dyn_loss = F.mse_loss(s_pred_tp1, s_true_tp1)
+        total_dyn_loss += dyn_loss
+
+        for i in range(N):
+            s_i_t = s_t[:, i]         # (B, 4)
+            a_i_t = actions[:, i]     # (B, m)
+            c_i = cost_embeddings[i].unsqueeze(0).expand(B, -1)  # (B, k)
+
+            s_i_t.requires_grad_(True)
+            a_i_t.requires_grad_(True)
+            c_i.requires_grad_(True)
+
+            cost = cost_net(s_t_flat, a_i_t, c_i).squeeze(-1)  # (B,)
+
+            grad_a = torch.autograd.grad(cost.sum(), a_i_t, create_graph=True)[0]  # (B, m)
+
+            hess_diag = []
+            for j in range(a_i_t.shape[1]):
+                grad2 = torch.autograd.grad(grad_a[:, j].sum(), a_i_t, retain_graph=True)[0][:, j]
+                hess_diag.append(grad2)
+            hess_diag = torch.stack(hess_diag, dim=1)  # (B, m)
+
+            log_det_Q = torch.log(torch.clamp(hess_diag, min=1e-6)).sum(dim=1)  # (B,)
+            ece_loss = cost + entropy_weight * log_det_Q
+            total_ece_loss += ece_loss.mean()
+
+        v_t = s_t[:, :, 2:]
+        v_tp1 = s_tp1[:, :, 2:]
+        accel = (v_tp1 - v_t)
+        accel_loss = accel.square().sum(dim=-1).mean()
+        total_accel_loss += accel_loss
+
+    embed_loss = (cost_embeddings.norm(dim=-1) - 1).square().mean()
+
+    total_loss = (
+        total_dyn_loss +
+        ece_weight * total_ece_loss +
+        accel_weight * total_accel_loss +
+        embed_weight * embed_loss
+    )
+
+    return total_loss
+
+
+cost_net = CostNet()
+dynamics_net = DynamicsNet()
+cost_embeddings = nn.Parameter(torch.randn(N, k))
+policy_net = PolicyNet(state_dim=4, cost_dim=k, action_dim=m)
+
+optimizer = torch.optim.Adam(list(cost_net.parameters()) +
+                             list(dynamics_net.parameters()) +
+                             list(policy_net.parameters()) +
+                             [cost_embeddings], lr=1e-3)
+
+
+num_epochs = 100
+
+# TODO
+# data = TODO
+
+for epoch in range(num_epochs):
+    for batch in data:
+        loss = training_step(batch, cost_net, dynamics_net, cost_embeddings)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
